62d052b21b34f1e42671d946b64928e8433aa0ee
Hesham Amoudi <heshamamoudi.it@gmail.com>
2023-12-10 01:02:04 +0300

    test 2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-// app.use("/",(req, res)=>{
-//     return "<p>hello world</p>"
-// })
+app.use("/getter",(req, res)=>{
+    return "<p>hello world</p>"
+})
 let port = process.env.PORT || 8080
 app.listen(port,()=>{
     console.log(`app is running on ${port}`)
